1e27a43130fc098457e6155274158f25c4713789
Abraham E. Tavarez <abetavarez@icloud.com>
2022-07-28 19:29:55 -0400

    bottom and side components layput with icons

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,7 +1,11 @@
 import React, { useRef, useState } from 'react';
-import { View, Text, TouchableWithoutFeedback } from 'react-native';
+import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
 import { Video } from 'expo-av';
 import styles from './styles';
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 const Post = () => {
   const video = useRef(null);
@@ -17,7 +21,7 @@ const Post = () => {
           ref={video}
           style={styles.video}
           source={{
-            uri: 'https://v16m-webapp.tiktokcdn-us.com/cbc58fa5070cbf9df8521f032eaa3397/62e308d3/video/tos/useast5/tos-useast5-ve-0068c002-tx/bc908eff201745b79aae796120294f9c/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1786&bt=893&cs=0&ds=3&ft=ebtHKH-qMyq8ZBTf5we2Njo7fl7Gb&mime_type=video_mp4&qs=0&rc=OjM3PGk4ZzloZjg5O2g0OUBpam1xOzU6ZmczZTMzZzczNEA0X2E0NWE2Xi4xYTEtYTMtYSNuMXJtcjQwLWJgLS1kMS9zcw%3D%3D&l=20220728160655639A5C3290444A33E07F'
+            uri: 'https://v16m-webapp.tiktokcdn-us.com/dcbc35d1cf69b0518861c0266de06c09/62e34128/video/tos/useast5/tos-useast5-ve-0068c003-tx/c3d2d7761f174023851bf3e18a2deaf8/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1452&bt=726&cs=0&ds=3&ft=ebtHKH-qMyq8ZfJq5we2NzHGfl7Gb&mime_type=video_mp4&qs=0&rc=ZWRoZDw7MzU1M2YzNDw8OUBpanNweDw6ZjtvZTMzZzczNEBiXmAwNjYtXjQxXmJhMV9gYSNmazRicjRfbDJgLS1kMS9zcw%3D%3D&l=20220728200737538C1D6803E5C80D1A8F'
           }}
           onError={(e) => console.error(e)}
           useNativeControls="false"
@@ -27,8 +31,42 @@ const Post = () => {
           onPlaybackStatusUpdate={(status) => setStatus(() => status)}
         />
       </TouchableWithoutFeedback>
+
+      <View style={styles.uiContainer}>
+        <View style={styles.sideContainer}>
+          <View style={styles.profilePictureContainer}>
+            <Image
+              style={styles.profilePicture}
+              source={{
+                uri: 'https://foreignpolicyi.org/wp-content/uploads/2019/06/Dwayne-Johnson.jpg'
+              }}
+            />
+          </View>
+          <View style={styles.iconsContainer}>
+            <AntDesign name={'hearto'} size={40} color="white" />
+            <Text style={styles.statsLabel}>112</Text>
+
+            <View style={styles.iconsContainer}>
+              <FontAwesome name={'commenting'} size={40} color="white" />
+              <Text style={styles.statsLabel}>112</Text>
+            </View>
+
+            <View style={styles.iconsContainer}>
+              <Fontisto name={'share-a'} size={40} color="white" />
+              <Text style={styles.statsLabel}>112</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+
       <View style={styles.bottomContainer}>
-        <Text>This is the bottom component</Text>
+        <Text style={styles.handle}>@abedev</Text>
+        <Text style={styles.description}>Workout Monday!</Text>
+
+        <View style={styles.songRow}>
+          <Entypo name={'beamed-note'} size={24} color="white" />
+          <Text style={styles.songName}>Nirvana - Something in the way!</Text>
+        </View>
       </View>
     </View>
   );
--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -2,17 +2,75 @@ import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    // marginTop: 80
-    // width: '100%'
-    // height: Dimensions.get('window').height
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
   },
   video: {
+    height: Dimensions.get('window').height,
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height
-    // position: 'absolute'
-    // top: 100,
-    // left: 0,
-    // right: 0
+    position: 'absolute',
+    zIndex: 1
+  },
+  uiContainer: {
+    position: 'absolute',
+    zIndex: 3,
+    bottom: 0,
+    top: '50%',
+    right: 0
+  },
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    padding: 10,
+    zIndex: 3
+  },
+  handle: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '700',
+    marginBottom: 10
+  },
+  description: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '300',
+    marginBottom: 10
+  },
+  songRow: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  songName: {
+    color: '#fff',
+    fontSize: 16,
+    marginLeft: 5
+  },
+  sideContainer: {
+    height: 300,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center'
+    // backgroundColor: 'yellow'
+  },
+  profilePicture: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: '#fff',
+    marginBottom: 20
+  },
+  iconsContainer: {
+    // height: 300,
+    // backgroundColor: 'red',
+    alignItems: 'center'
+  },
+  statsLabel: {
+    color: '#fff',
+    marginBottom: 20,
+    fontSize: 16,
+    fontWeight: '600'
   }
 });
 
