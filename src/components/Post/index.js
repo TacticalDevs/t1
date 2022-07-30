0e410868545ca0e0a86ee7cfa5ee01325cfc747d
Abraham E. Tavarez <abetavarez@icloud.com>
2022-07-30 19:04:40 -0400

    dummy data created at Home.js, Post component is consuming the dummy data, also like functionality is handle in the Post component local state

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,5 +1,11 @@
 import React, { useRef, useState } from 'react';
-import { View, Text, TouchableWithoutFeedback, Image } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableWithoutFeedback,
+  Image,
+  TouchableOpacity
+} from 'react-native';
 import { Video } from 'expo-av';
 import styles from './styles';
 import Entypo from 'react-native-vector-icons/Entypo';
@@ -7,29 +13,42 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Fontisto from 'react-native-vector-icons/Fontisto';
 
-const Post = () => {
+const Post = (props) => {
+  // const { post } = props;
+  const [post, setPost] = useState(props.post);
   const video = useRef(null);
   const [status, setStatus] = useState({});
+  const [isLiked, setIsLiked] = useState(false);
+  // console.log(post);
 
   const handlePlayPausePress = () => {
     status.isPlaying ? video.current.pauseAsync() : video.current.playAsync();
   };
+
+  const onLikePress = () => {
+    const changeLike = isLiked ? -1 : 1;
+    setPost({ ...post, likes: post.likes + changeLike });
+    setIsLiked(!isLiked);
+  };
+
   return (
     <View style={styles.container}>
       <TouchableWithoutFeedback onPress={handlePlayPausePress}>
-        <Video
-          ref={video}
-          style={styles.video}
-          source={{
-            uri: 'https://v16m-webapp.tiktokcdn-us.com/dcbc35d1cf69b0518861c0266de06c09/62e34128/video/tos/useast5/tos-useast5-ve-0068c003-tx/c3d2d7761f174023851bf3e18a2deaf8/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=1452&bt=726&cs=0&ds=3&ft=ebtHKH-qMyq8ZfJq5we2NzHGfl7Gb&mime_type=video_mp4&qs=0&rc=ZWRoZDw7MzU1M2YzNDw8OUBpanNweDw6ZjtvZTMzZzczNEBiXmAwNjYtXjQxXmJhMV9gYSNmazRicjRfbDJgLS1kMS9zcw%3D%3D&l=20220728200737538C1D6803E5C80D1A8F'
-          }}
-          onError={(e) => console.error(e)}
-          useNativeControls="false"
-          resizeMode="cover"
-          // isLooping="true"
-          shouldPlay="true"
-          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
-        />
+        {post && (
+          <Video
+            ref={video}
+            style={styles.video}
+            source={{
+              uri: post.videoUri
+            }}
+            onError={(e) => console.error(e)}
+            useNativeControls="false"
+            resizeMode="cover"
+            // isLooping="true"
+            shouldPlay="true"
+            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+          />
+        )}
       </TouchableWithoutFeedback>
 
       <View style={styles.uiContainer}>
@@ -38,46 +57,51 @@ const Post = () => {
             <Image
               style={styles.profilePicture}
               source={{
-                uri: 'https://foreignpolicyi.org/wp-content/uploads/2019/06/Dwayne-Johnson.jpg'
+                uri: post.user.userUri
               }}
             />
           </View>
+
+          <TouchableOpacity style={styles.iconsContainer} onPress={onLikePress}>
+            <AntDesign
+              name={'heart'}
+              size={40}
+              color={isLiked ? 'red' : 'white'}
+            />
+            <Text style={styles.statsLabel}>{post.likes}</Text>
+          </TouchableOpacity>
+
           <View style={styles.iconsContainer}>
-            <AntDesign name={'heart'} size={40} color="white" />
-            <Text style={styles.statsLabel}>2598</Text>
+            <FontAwesome name={'commenting'} size={40} color="white" />
+            <Text style={styles.statsLabel}>{post.comments}</Text>
+          </View>
 
-            <View style={styles.iconsContainer}>
-              <FontAwesome name={'commenting'} size={40} color="white" />
-              <Text style={styles.statsLabel}>867</Text>
-            </View>
+          <View style={styles.iconsContainer}>
+            <FontAwesome name={'bookmark'} size={40} color="white" />
+            <Text style={styles.statsLabel}>{post.bookmarks}</Text>
+          </View>
 
-            <View style={styles.iconsContainer}>
-              <FontAwesome name={'bookmark'} size={40} color="white" />
-              <Text style={styles.statsLabel}>117</Text>
-            </View>
-
-            <View style={styles.iconsContainer}>
-              <Fontisto name={'share-a'} size={35} color="white" />
-              <Text style={styles.statsLabel}>443</Text>
-            </View>
+          <View style={styles.iconsContainer}>
+            <Fontisto name={'share-a'} size={35} color="white" />
+            <Text style={styles.statsLabel}>{post.shares}</Text>
           </View>
         </View>
       </View>
 
       <View style={styles.bottomContainer}>
         <View>
-          <Text style={styles.handle}>@abedev</Text>
-          <Text style={styles.description}>Workout Monday!</Text>
+          <Text style={styles.handle}>@{post.user.username}</Text>
+          <Text style={styles.description}>{post.description}</Text>
 
           <View style={styles.songRow}>
             <Entypo name={'beamed-note'} size={24} color="white" />
-            <Text style={styles.songName}>Nirvana - Something in the way!</Text>
+            <Text style={styles.songName}>{post.song}</Text>
 
             <View style={styles.songImageContainer}>
               <Image
                 style={styles.songImage}
                 source={{
-                  uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.sundazed.com%2Fmedia%2Fimages%2FLP-5063_BLK_900.png&f=1&nofb=1'
+                  uri: post.songImage
                 }}
               />
             </View>
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -3,9 +3,28 @@ import { View, Text } from 'react-native';
 import Post from '../../components/Post';
 
 const Home = () => {
+  const post1 = {
+    id: 'p1',
+    videoUri:
+      'https://d8vywknz0hvjw.cloudfront.net/fitenium-media-prod/videos/45fee890-a74f-11ea-8725-311975ea9616/proccessed_720.mp4',
+    user: {
+      id: 'u1',
+      username: 'TheRock',
+      userUri:
+        'https://foreignpolicyi.org/wp-content/uploads/2019/06/Dwayne-Johnson.jpg'
+    },
+    description: 'Monday Workout!',
+    song: 'Nirvana - Come as you are!',
+    songImage:
+      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.sundazed.com%2Fmedia%2Fimages%2FLP-5063_BLK_900.png&f=1&nofb=1',
+    likes: 2598,
+    comments: 867,
+    bookmarks: 117,
+    shares: 443
+  };
   return (
     <View>
-      <Post />
+      <Post post={post1} />
     </View>
   );
 };
